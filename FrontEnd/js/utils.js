2a59d7fe64801888867f32ba7f289488f66936a1
Myl-W <mylene.wan@gmail.com>
2023-06-22 17:04:25 +0200

    event

--- FrontEnd/js/utils.js
+++ FrontEnd/js/utils.js
@@ -338,18 +338,16 @@ export function creatForm(modalContainer) {
     });
 
     boutonValider.addEventListener('click', async (event) => {
+        event.preventDefault()
         const fileInput = document.getElementById("image");
         const titreInput = document.getElementById("titre");
         const categorieSelect = document.getElementById("categories-select");
 
         if (!fileInput.value) {
-            event.preventDefault();
             alert("Veuillez sélectionner un fichier.");
         } else if (!titreInput.value) {
-            event.preventDefault();
             alert("Veuillez entrer un titre pour votre fichier.");
         } else if (categorieSelect.value < 1) {
-            event.preventDefault();
             alert("Veuillez sélectionner une catégorie pour votre fichier.");
         } else {
             addWork();
@@ -546,25 +544,6 @@ export function createImagePreview() {
 //crée un bouton pour ajouter une photo et gère l'événement associé
 export function createBoutonAjouterPhoto(modalFormDiv, inputFormFile) {
     const boutonAjouterPhoto = createButtonWithClassAndText('bouton-ajouter-photo', '+ Ajouter photo');
-    // Ajoute un écouteur d'événement au bouton pour le clic
-    boutonAjouterPhoto.addEventListener('click', (e) => {
-        e.preventDefault(); // Empêche le comportement par défaut du clic
-        inputFormFile.click();
-        // Obtient tous les éléments avec la classe 'input-file'
-        const inputFiles = document.getElementsByClassName('input-file');
-        // Parcourt tous les éléments inputFiles et modifie le style pour ajuster les marges
-        for (let i = 0; i < inputFiles.length; i++) {
-            inputFiles[i].style.paddingTop = '0px';
-            inputFiles[i].style.paddingBottom = '0px';
-        }
-        console.log(inputFiles); // Affiche les éléments inputFiles dans la console
-        // Sélectionne l'élément avec la classe 'picture' et le masque
-        const i = document.querySelector('.picture');
-        const span = document.querySelector('.text-caption');
-        boutonAjouterPhoto.style.display = 'none'; // Masque le bouton boutonAjouterPhoto
-        i.style.display = 'none'; // Masque l'élément i (image)
-        span.style.display = 'none'; // Masque l'élément span (légende)
-    });
     return boutonAjouterPhoto; // Retourne le bouton boutonAjouterPhoto
 }
 
